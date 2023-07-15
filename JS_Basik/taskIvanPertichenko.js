977756c48be35db83eab81730857fd892d2e93df
Alexey <alexledovit@gmail.com>
2023-07-15 21:20:26 +0300

    add: new solution task

--- JS_Basik/taskIvanPertichenko.js
+++ JS_Basik/taskIvanPertichenko.js
@@ -106,3 +106,130 @@ function fifthTask() {
     return arrayOfNumbers;
 }
 fifthTask();
+
+
+/* Тема урока : Продвинутые задания на использование циклов и условий */
+//----------Зачада 1 ----------------
+/* Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+Должен получиться точно такой же массив
+ */
+
+// Место для первой задачи
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+   
+    // Пишем решение вот тут
+   for(i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+   }
+    //result.push(arr);
+    console.log(result);
+    // Не трогаем
+    return result;
+}
+
+firstTask();
+
+//---------------------------Задача 2 ----------------------
+/* 
+Измените данный массив так, чтобы все числа были увеличены в 2 раза, 
+а если попадается строка строка - то к ней было добавлено " - done".
+Для определения типа данных используйте typeof();
+Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ] */
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+
+    for(i = 0; i < data.length; i++){
+        if(typeof data[i] == 'number') {
+            data[0] = 10;
+            data[1] = 20;
+            data[3] = 40;  
+        }
+
+        if(typeof data[i] == 'string') {
+            data[2] = 'Shopping - done';
+            data[4] = 'Homework - done';
+        }
+    }
+   console.log(data);
+    
+    // Не трогаем
+    return data;
+}secondTask();
+
+
+//--- Правильное решение 
+function secondTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        }
+    }
+
+    console.log(data);
+    return data;
+}
+
+secondTask();
+
+
+//------- Задача 3 ------------------
+/*
+Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
+Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+ */
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+    // Пишем решение вот тут
+    for(i = 0; i < data.length; i++) {
+        data.reverse([i]);
+    }
+    result.push(data);
+    console.log(result.flat());
+
+    return result;
+    
+
+    for(i = 0; i < data.length; i++) {
+        result[0] = 'Homework';
+        result[1] = 20 ;
+        result[2] = 'Shopping';
+        result[3] = 10;
+        result[4] = 5;
+    }
+   
+   console.log(result);
+  
+    // Не трогаем
+    return result;
+}
+
+//---Правльное решение 
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i]
+    }
+
+    console.log(result);
+    return result;
+}
+
+thirdTask();
