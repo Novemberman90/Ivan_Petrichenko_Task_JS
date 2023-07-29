3a107c57d1fc8f8085432aff426693643a0f7fb2
Alexey <alexledovit@gmail.com>
2023-07-29 20:41:30 +0300

    refactor: added 5 new function
    start, rememberMyFilms, detectPersonalLevel, showMyBD,
    writeYourGenres

--- JS_Basik/cinemaTask.js
+++ JS_Basik/cinemaTask.js
@@ -11,9 +11,9 @@ function start() {
 // не будет введено число
     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели ?", '');
-    };
+    }
 
-};
+}
 start();
 
 let personalMovieDB = {
@@ -24,37 +24,31 @@ let personalMovieDB = {
     privat : false,
 };
 
-const question1 = prompt("Один из последних просмотренных фильмов ?", '');
-const question2 = +prompt("На сколько вы его оцените ?", '');
+//const question1 = prompt("Один из последних просмотренных фильмов ?", '');
+//const question2 = +prompt("На сколько вы его оцените ?", '');
     
-      personalMovieDB.movies[question1] = question2;
-     
+      //personalMovieDB.movies[question1] = question2;
 
-for (i=0; i < 2; i++) {
-    const question1 = prompt("Один из последних просмотренных фильмов ?",'');
-    const question2 = +prompt("На сколько вы его оцените ?", '');
-personalMovieDB.movies[question1] = question2;
-// если не нажал "Отмена" в question1 и question2 и не оставил пустую строку и меньше 10 символов тогда ок
-    if( question1 != null && question2 != null && question1 != '' && question2 != ''
-    && question1.length < 10) {
-        personalMovieDB.movies[question1] = question2;
-        alert ("Done")
-    } else { 
-        alert('Ещё раз !!!')
-         i--; // если не ответил, то мы вернемся на один вопрос назад и так будет каждый раз.
-    }; 
-    
-    /*if((question1 == null || question2 == null) || question1.length < 5 || question2.length < 5){
-        alert("Вы что ничего не указали ? Спросим вас ещё раз!");
-        question1 = prompt("Один из последних просмотренных фильмов ?!!!",''),
-         question2 = prompt("На сколько вы его оцените ?", '');
-        
-} else {
-    alert("Молодец!");
-}; */
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const question1 = prompt("Один из последних просмотренных фильмов ?",'');
+        const question2 = +prompt("На сколько вы его оцените ?", '');
+         personalMovieDB.movies[question1] = question2;
+    // если не нажал "Отмена" в question1 и question2 и не оставил пустую строку и меньше 10 символов тогда ок
+        if( question1 != null && question2 != null && question1 != '' && question2 != ''
+        && question1.length > 1) {
+            personalMovieDB.movies[question1] = question2;
+            alert ("Done");
+        } else { 
+            alert('Ещё раз !!!');
+             i--; // если не ответил, то мы вернемся на один вопрос назад и так будет каждый раз.
+        } 
+    }
 }
+rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log ('Вы классический зритель');
@@ -63,5 +57,29 @@ if (personalMovieDB.count < 10) {
 } else {
     console.log('Произошла ошибка');
 }
+}
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyBD(hidden){
+        if(!hidden)
+          console.log(personalMovieDB);
+}
+ showMyBD(personalMovieDB.privat);
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++) {
+        const question3 = prompt (`Ваш любимый жанр под номерм ${i}`);
+        personalMovieDB.generes[i-1] = question3;
+        /* 
+         в таком случаии в массиве будет ровно 3 значения. Если начнем с 0 тогда в списке будет 4 значения,
+         но самое первое значение будет пустое. 
+
+         ещё один вариант записи 
+            personalMovieDB.generes[i-1] = prompt (`Ваш любимый жанр под номерм ${i}`);
+         */
+
+        
+    }
+}
+writeYourGenres();
